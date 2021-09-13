99ab5938539ccf47282a8f74d8f0828c551244d4
Alef Felix <aleffelix@MacBook-Air.Dlink>
2021-09-13 09:38:47 -0300

    Aula 2

---
+++ aulas/aula02/gdrive-webapi/src/uploadHandler.js
@@ -0,0 +1,70 @@
+import fs from 'fs'
+import Busboy from 'busboy'
+import { pipeline } from 'stream/promises'
+
+import { logger } from './logger'
+
+export default class UploadHandler {
+
+    constructor({ io, socketId, downloadsFolder, messageTimeDelay = 200 }) {
+        Object.assign(this, {
+            io,
+            socketId,
+            downloadsFolder,
+            messageTimeDelay,
+            ON_UPLOAD_EVENT: 'file-upload',
+        })
+    }
+    
+    canExecute(lastExecution) {
+        return (Date.now() - lastExecution) >= this.messageTimeDelay
+    }
+
+    handleFileBytes(filename) {
+        this.lastMessageSent = Date.now()
+
+        async function* handleData(source) {
+            let processedAlready = 0
+            for await(const chunk of source) {
+                yield chunk
+
+                processedAlready += chunk.length
+                
+                if (!this.canExecute(this.lastMessageSent)) {
+                    continue;
+                }
+
+                this.lastMessageSent = Date.now()
+                this.io.to(this.socketId).emit(this.ON_UPLOAD_EVENT, {
+                    processedAlready,
+                    filename,
+                })
+                logger.info(`File [${filename}] got ${processedAlready} bytes to ${this.socketId}`)
+            }
+        }
+
+        return handleData.bind(this)
+    }
+
+    async onFile(fieldname, file, filename) {
+        const saveTo = `${this.downloadsFolder}/${filename}`
+
+        await pipeline(
+            file,
+            this.handleFileBytes.apply(this, [filename]),
+            fs.createWriteStream(saveTo),
+        )
+
+        logger.info(`File [${filename}] finished`)
+    }
+
+    registerEvents(headers, onFinish) {
+        const busboy = new Busboy({ headers })
+
+        busboy.on('file', this.onFile.bind(this))
+        busboy.on('finish', onFinish)
+
+        return busboy
+    }
+
+}
